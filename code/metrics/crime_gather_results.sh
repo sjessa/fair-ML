dfaa2148b2193b124ada26d718bfd487064ee134
Adam Cavatassi <[adamcavatassi@gmail.com]>
2017-04-16 14:57:56 -0400

    adding Ratios of raw recidivism data

---
+++ code/metrics/crime_gather_results.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+echo "gathering in process..... please wait"
+
+echo ,IMPACT RATIO:,ELIFT RATIO:,ODDS RATIO:, > crime_comparison.csv
+echo -n Raw Data, >> crime_comparison.csv 
+python metrics.py black crime crime_clean.csv False | cut -d' ' -f3 | paste -sd ',' >> crime_comparison.csv
+
+find ../output -maxdepth 1 -name "*crime*" -print > predictions_list.txt
+find ../output -maxdepth 1 -name "*pred*" -print >> predictions_list.txt
+
+while read prediction
+do
+    echo -n "$prediction", >> crime_comparison.csv
+    if [[ ${prediction} != *"pred"* ]];then
+	python metrics.py black crime "$prediction" False | cut -d' ' -f3 | paste -sd ',' >> crime_comparison.csv
+    else
+   	python metrics.py black pred "$prediction" False | cut -d' ' -f3 | paste -sd ',' >> crime_comparison.csv
+    fi
+done < predictions_list.txt
+
+echo ,ACCURACY:,PRECISION:,RECALL:,F1 SCORE:, > crime_accuracy.csv
+
+rm predictions_list.txt
+find ../output -maxdepth 1 -name "*pred*" -print > predictions_list.txt
+
+while read prediction2
+do
+    echo -n "$prediction2", >> crime_accuracy.csv
+    python accuracy.py pred class "$prediction2" | cut -d' ' -f2 | paste -sd ',' >> crime_accuracy.csv
+  
+done < predictions_list.txt
+
+echo "all done!"
